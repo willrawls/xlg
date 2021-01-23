7c9586aa5edc5cef460846b4e8a8bd752718a077
William M. Rawls <william.rawls@gmail.com>
2021-01-22 22:35:35 -0800

    MetX.Techniques.Tests compiles in 5.0

--- MetX/MetX.QuickScripts/Properties/AssemblyInfo.cs
+++ MetX/MetX.QuickScripts/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("fc4f34c4-7dcc-4101-82ed-6ee63bca7ed5")]
-[assembly: AssemblyVersion("2.2.0.*")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("5.0.1")]
+[assembly: AssemblyFileVersion("5.0.1")]
