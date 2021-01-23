8926d8d099ba09ef71b397a96acfc70697ec20d5
William M. Rawls <william.rawls@gmail.com>
2021-01-22 23:24:57 -0800

    Oh god... 5.0 doesn't support the code provider compiler (sob)
    Searching for an alternative or this will be an abandoned line of code... frell. Why would they do that? I mean, I know why and the what fors but taking the ability to dynamically compile and run code, in memory is just... (sigh)

--- MetX/MetX/Scripts/XlgQuickScript.cs
+++ MetX/MetX/Scripts/XlgQuickScript.cs
@@ -1,5 +1,8 @@
 //using NArrange.Core.Configuration;
 
+using System.Globalization;
+using System.IO;
+
 namespace MetX.Scripts
 {
     using System;
@@ -84,7 +87,7 @@ namespace MetX.Scripts
                 compilerParameters.OutputAssembly =
                     Guid.NewGuid().ToString().Replace(new[] { "{", "}", "-" }, string.Empty).ToLower() + ".exe";
             }
-
+            
             compilerParameters
                 .ReferencedAssemblies
                 .AddRange(
@@ -101,11 +104,103 @@ namespace MetX.Scripts
                         .ToArray());
             }
 
-            var compilerResults = new CSharpCodeProvider().CompileAssemblyFromSource(compilerParameters,
-                source);
+            var cSharpProvider = CodeDomProvider.CreateProvider("CSharp");
+            /*
+            var cp = new CompilerParameters
+            {
+                GenerateExecutable = true,
+                OutputAssembly = exeName,
+                GenerateInMemory = false,
+                TreatWarningsAsErrors = false
+            };
+            */
+
+            var compilerResults = cSharpProvider.CompileAssemblyFromSource(compilerParameters, source);
+            //var compilerResults = new CSharpCodeProvider().CompileAssemblyFromSource(compilerParameters, source);
             return compilerResults;
         }
 
+        public static bool CompileExecutable(String sourceName)
+        {
+            FileInfo sourceFile = new FileInfo(sourceName);
+            CodeDomProvider provider = null;
+            bool compileOk = false;
+
+            // Select the code provider based on the input file extension.
+            if (sourceFile.Extension.ToUpper(CultureInfo.InvariantCulture) == ".CS")
+            {
+                provider = CodeDomProvider.CreateProvider("CSharp");
+            }
+            else if (sourceFile.Extension.ToUpper(CultureInfo.InvariantCulture) == ".VB")
+            {
+                provider = CodeDomProvider.CreateProvider("VisualBasic");
+            }
+            else
+            {
+                Console.WriteLine("Source file must have a .cs or .vb extension");
+            }
+
+            if (provider != null)
+            {
+
+                // Format the executable file name.
+                // Build the output assembly path using the current directory
+                // and <source>_cs.exe or <source>_vb.exe.
+
+                String exeName = String.Format(@"{0}\{1}.exe",
+                    System.Environment.CurrentDirectory,
+                    sourceFile.Name.Replace(".", "_"));
+
+                CompilerParameters cp = new CompilerParameters();
+
+                // Generate an executable instead of
+                // a class library.
+                cp.GenerateExecutable = true;
+
+                // Specify the assembly file name to generate.
+                cp.OutputAssembly = exeName;
+
+                // Save the assembly as a physical file.
+                cp.GenerateInMemory = false;
+
+                // Set whether to treat all warnings as errors.
+                cp.TreatWarningsAsErrors = false;
+
+                // Invoke compilation of the source file.
+                CompilerResults cr = provider.CompileAssemblyFromFile(cp,
+                    sourceName);
+
+                if(cr.Errors.Count > 0)
+                {
+                    // Display compilation errors.
+                    Console.WriteLine("Errors building {0} into {1}",
+                        sourceName, cr.PathToAssembly);
+                    foreach(CompilerError ce in cr.Errors)
+                    {
+                        Console.WriteLine("  {0}", ce.ToString());
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    // Display a successful compilation message.
+                    Console.WriteLine("Source {0} built into {1} successfully.",
+                        sourceName, cr.PathToAssembly);
+                }
+
+                // Return the results of the compilation.
+                if (cr.Errors.Count > 0)
+                {
+                    compileOk = false;
+                }
+                else
+                {
+                    compileOk = true;
+                }
+            }
+            return compileOk;
+        }
+        
         public static string ExpandScriptLineToSourceCode(string currScriptLine, int indent)
         {
             // backslash percent will translate to % after parsing
