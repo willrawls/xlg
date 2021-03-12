b920c105a7c78e93e589855b06232ab4600e3017
William M. Rawls <william.rawls@gmail.com>
2021-03-12 08:03:00 -0800

    Bank

--- MetX/MetX.Generators/Program.cs
+++ MetX/MetX.Generators/Program.cs
@@ -6,7 +6,32 @@ namespace MetX.Generators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  Generates generator in current folder
+            //  MetX.Generators.exe -generate Namespace.GeneratorName 
+            //  When sub folders are missing or are empty
+            //      Creates 2 sub folders
+            //          Namespace
+            //              Namespace.csproj
+            //              GeneratorName.cs
+            //          Namespace.Aspects
+            //              Namespace.Aspects.csproj
+            //              GeneratorNameAttribute.cs
+            //  When sub folder already exist
+            //      Add or overwrite
+            //          Namespace
+            //              Namespace.csproj
+            //                  Create NextGeneratorName.cs
+            //                  Add NextGeneratorName.cs to .csproj
+            //              Namespace.NextGeneratorName.cs
+            //          Namespace.Aspects
+            //              Namespace.Name.Aspects.csproj
+            //              NextGeneratorNameAttribute.cs
+            //  MetX.Generators.exe -addto "PathTo.csproj the generator should be added as an analyzer"
+            //      
+            //  MetX.Generators.exe -just Namespace.GeneratorName -addto "PathTo.csproj the generator should be added as an analyzer"
+            //      Find and modify/update the csproj of the client
+            
+            Console.WriteLine("Ding 3");
         }
     }
 }
