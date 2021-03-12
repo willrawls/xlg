c66ec434f344a51c2a1fd0d080090b988339f701
William M. Rawls <william.rawls@gmail.com>
2021-03-11 22:45:20 -0800

    Skeleton for GenerateFromTemplate

--- MetX/MetX.Aspects/GenerateAddingStaticCode.cs
+++ MetX/MetX.Aspects/GenerateAddingStaticCode.cs
@@ -7,4 +7,15 @@ namespace MetX.Aspects
     {
         
     }
+    
+    [AttributeUsage(AttributeTargets.Class)]
+    public class GenerateFromTemplate : Attribute 
+    {
+        public readonly string TemplateFilePath;
+
+        public GenerateFromTemplate(string templateFilePath)
+        {
+            TemplateFilePath = templateFilePath;
+        }
+    }
 }
--- MetX/MetX.Generators.Samples.Client/Program.cs
+++ MetX/MetX.Generators.Samples.Client/Program.cs
@@ -7,11 +7,15 @@ namespace MetX.Generators.Samples.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            MetX.Generated.GClass.GMethod();
+            Generated.GClass.GMethod();
+            Generated.FredExampleOne.Go();
         }
     }
 
     [MetX.Aspects.GenerateAddingStaticCode]
+    public class EmptyClass { }
+    
+    [Aspects.GenerateFromTemplate("exampleOne.template")]
     public class Fred
     {
         public string George;
