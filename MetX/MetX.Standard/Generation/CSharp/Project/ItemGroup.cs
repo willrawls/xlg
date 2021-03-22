e732b3e14c95b5195f433888a3e6d3858d87f2d1
William M. Rawls <william.rawls@gmail.com>
2021-03-21 18:04:49 -0700

    Green

---
+++ MetX/MetX.Standard/Generation/CSharp/Project/ItemGroup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace MetX.Standard.Generation.CSharp.Project
+{
+    public class ItemGroup
+    {
+        public Modifier Parent { get; }
+        public List<PackageReference> PackageReferences;
+        
+        // Microsoft.CodeAnalysis.Common
+        public PackageReference Common;
+        
+        // Microsoft.CodeAnalysis.Analyzers
+        public PackageReference Analyzers;
+        
+        // Microsoft.CodeAnalysis.CSharp.Workspaces
+        public PackageReference CSharpWorkspaces;
+        
+        public ItemGroup(Modifier parent)
+        {
+            Parent = parent;
+            Common = new PackageReference(parent, "Microsoft.CodeAnalysis.Common", "3.9.0");
+            Analyzers = new PackageReference(parent, "Microsoft.CodeAnalysis.Analyzers", "3.3.2",
+                "all", "runtime; build; native; contentfiles; analyzers; buildtransitive");
+            CSharpWorkspaces = new PackageReference(parent, "Microsoft.CodeAnalysis.CSharp.Workspaces", "3.9.0", "all");
+
+            PackageReferences = new List<PackageReference>
+            {
+                Common,
+                Analyzers,
+                CSharpWorkspaces,
+            };
+        }
+    }
+}
--- MetX/MetX.Tests/Standard/Generation/CSharp/Project/ModifierTests_Project_PackageReference.cs
+++ MetX/MetX.Tests/Standard/Generation/CSharp/Project/ModifierTests_Project_PackageReference.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Xml;
 using MetX.Standard.Generation.CSharp.Project;
+using MetX.Standard.Library;
 using MetX.Tests.Standard.Generation.CSharp.Project.Pieces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,71 +15,118 @@ namespace MetX.Tests.Standard.Generation.CSharp.Project
         public void InsertOne_NotPresent()
         {
             var modifier = Piece.GetEmptyClient();
-            var packageName = "Microsoft.CodeAnalysis.Common";
-            var version = "3.9.0";
-            var packageReference = modifier.ItemGroup.PackageReference.InsertOrUpdate(packageName, version);
+            var element = modifier.ItemGroup.Common.InsertOrUpdate();
             
-            Assert.IsNotNull(packageReference);
-            Assert.AreEqual(packageName, packageReference.Attributes["Include"]?.Value);
-            Assert.IsNotNull(version, packageReference.Attributes["Version"]?.Value);
+            Assert.IsNotNull(element);
+            Assert.AreEqual(modifier.ItemGroup.Common.PackageName, element.Attributes["Include"]?.Value);
+            Assert.IsNotNull(modifier.ItemGroup.Common.Version, element.Attributes["Version"]?.Value);
         }
         
         [TestMethod]
         public void InsertOneWithAssets_NotPresent()
         {
             var modifier = Piece.GetEmptyClient();
-            var packageName = "Microsoft.CodeAnalysis.Analyzers";
-            var version = "3.3.2";
-            var privateAssets = "all";
-            var includeAssets = "runtime; build; native; contentfiles; analyzers; buildtransitive";
-            var packageReference = modifier.ItemGroup.PackageReference.InsertOrUpdate(packageName, version, privateAssets, includeAssets);
+            var element = modifier.ItemGroup.Analyzers.InsertOrUpdate();
             
-            Assert.IsNotNull(packageReference);
-            Assert.AreEqual(packageName, packageReference.Attributes["Include"]?.Value);
-            Assert.AreEqual(version, packageReference.Attributes["Version"]?.Value);
+            Assert.IsNotNull(element);
+            Assert.AreEqual(modifier.ItemGroup.Analyzers.PackageName, element.Attributes["Include"]?.Value);
+            Assert.AreEqual(modifier.ItemGroup.Analyzers.Version, element.Attributes["Version"]?.Value);
             
-            Assert.AreEqual(2, packageReference.ChildNodes.Count);
-            Assert.AreEqual("PrivateAssets", packageReference.ChildNodes[0]?.Name);
-            Assert.AreEqual(privateAssets, packageReference.ChildNodes[0]?.InnerText);
+            Assert.AreEqual(2, element.ChildNodes.Count);
+            Assert.AreEqual("PrivateAssets", element.ChildNodes[0]?.Name);
+            Assert.AreEqual(modifier.ItemGroup.Analyzers.PrivateAssets, element.ChildNodes[0]?.InnerText);
             
-            Assert.AreEqual("IncludeAssets", packageReference.ChildNodes[1]?.Name);
-            Assert.AreEqual(includeAssets, packageReference.ChildNodes[1]?.InnerText);
+            Assert.AreEqual("IncludeAssets", element.ChildNodes[1]?.Name);
+            Assert.AreEqual(modifier.ItemGroup.Analyzers.IncludeAssets, element.ChildNodes[1]?.InnerText);
         }
         
         [TestMethod]
         public void InsertOne_Present()
         {
             var modifier = Piece.GetFullClient();
-            var packageName = "Microsoft.CodeAnalysis.Common";
-            var version = "3.9.0";
-            var packageReference = modifier.ItemGroup.PackageReference.InsertOrUpdate(packageName, version);
-            
-            Assert.IsNotNull(packageReference);
-            Assert.AreEqual(packageName, packageReference.Attributes["Include"]?.Value);
-            Assert.IsNotNull(version, packageReference.Attributes["Version"]?.Value);
+            var element = modifier.ItemGroup.Common.InsertOrUpdate();
+            AssertPackageReference(element, modifier.ItemGroup.Common);
         }
         
         [TestMethod]
         public void InsertOneWithAssets_Present()
         {
             var modifier = Piece.GetFullClient();
-            var packageName = "Microsoft.CodeAnalysis.Analyzers";
-            var version = "3.3.2";
-            var privateAssets = "all";
-            var includeAssets = "runtime; build; native; contentfiles; analyzers; buildtransitive";
-            var packageReference = modifier.ItemGroup.PackageReference.InsertOrUpdate(packageName, version, privateAssets, includeAssets);
-            
-            Assert.IsNotNull(packageReference);
-            Assert.AreEqual(packageName, packageReference.Attributes["Include"]?.Value);
-            Assert.AreEqual(version, packageReference.Attributes["Version"]?.Value);
-            
-            Assert.AreEqual(2, packageReference.ChildNodes.Count);
-            Assert.AreEqual("PrivateAssets", packageReference.ChildNodes[0]?.Name);
-            Assert.AreEqual(privateAssets, packageReference.ChildNodes[0]?.InnerText);
-            
-            Assert.AreEqual("IncludeAssets", packageReference.ChildNodes[1]?.Name);
-            Assert.AreEqual(includeAssets, packageReference.ChildNodes[1]?.InnerText);
+            var element = modifier.ItemGroup.Analyzers.InsertOrUpdate();
+            AssertPackageReference(element, modifier.ItemGroup.Analyzers);
         }
+
+        [TestMethod]
+        public void InsertOrUpdateAllForGeneration_Simple()
+        {
+            var modifier = Piece.GetFullClient();
+            var element = modifier.ItemGroup.Analyzers.InsertOrUpdate();
+            AssertPackageReference(element, modifier.ItemGroup.Analyzers);
+        }
+
+        [TestMethod]
+        public void InsertOrUpdateAll_Simple()
+        {
+            var modifier = Piece.GetEmptyClient();
+            
+            var analyzersPR = modifier.ItemGroup.Analyzers.InsertOrUpdate();
+            var commonPR = modifier.ItemGroup.Common.InsertOrUpdate();
+            var workspacesPR = modifier.ItemGroup.CSharpWorkspaces.InsertOrUpdate();
+
+            AssertPackageReference(analyzersPR, modifier.ItemGroup.Analyzers);
+            AssertPackageReference(commonPR, modifier.ItemGroup.Common);
+            AssertPackageReference(workspacesPR, modifier.ItemGroup.CSharpWorkspaces);
+        }
+
+        [TestMethod]
+        public void RemoveAll_AlreadyThere()
+        {
+            var modifier = Piece.GetFullClient();
+            
+            var analyzersPR = modifier.ItemGroup.Analyzers.Remove();
+            var commonPR = modifier.ItemGroup.Common.Remove();
+            var workspacesPR = modifier.ItemGroup.CSharpWorkspaces.Remove();
+            Assert.IsNull(analyzersPR);
+            Assert.IsNull(commonPR);
+            Assert.IsNull(workspacesPR);
+        }
+
+        [TestMethod]
+        public void RemoveAll_NothingThere()
+        {
+            var modifier = Piece.GetEmptyClient();
+            
+            var analyzersPR = modifier.ItemGroup.Analyzers.Remove();
+            var commonPR = modifier.ItemGroup.Common.Remove();
+            var workspacesPR = modifier.ItemGroup.CSharpWorkspaces.Remove();
+
+            Assert.IsNull(analyzersPR);
+            Assert.IsNull(commonPR);
+            Assert.IsNull(workspacesPR);
+        }
+
         
+        public static void AssertPackageReference(XmlElement element, PackageReference reference)
+        {
+            Assert.IsNotNull(element);
+            Assert.AreEqual(reference.PackageName, element.Attributes["Include"]?.Value);
+            Assert.AreEqual(reference.Version, element.Attributes["Version"]?.Value);
+
+            if(reference.PrivateAssets.IsNotEmpty())
+            {
+                Assert.IsTrue(element.ChildNodes.Count is > 0 and < 3);
+                Assert.AreEqual("PrivateAssets", element.ChildNodes[0]?.Name);
+                Assert.AreEqual(reference.PrivateAssets, element.ChildNodes[0]?.InnerText);
+            }
+            
+            if(reference.IncludeAssets.IsNotEmpty())
+            {
+                Assert.IsTrue(element.ChildNodes.Count is > 0 and < 3);
+                Assert.AreEqual("IncludeAssets", element.ChildNodes[1]?.Name);
+                Assert.AreEqual(reference.IncludeAssets, element.ChildNodes[1]?.InnerText);
+            }
+
+        }
+       
     }
 }
