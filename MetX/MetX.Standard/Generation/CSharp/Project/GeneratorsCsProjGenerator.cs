518eaf93f49322e72ce4181bee2131e7b90dd6b1
William M. Rawls <william.rawls@gmail.com>
2021-03-23 21:20:21 -0700

    Bank

---
+++ MetX/MetX.Standard/Generation/CSharp/Project/GeneratorsCsProjGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using MetX.Aspects;
+
+namespace MetX.Standard.Generation.CSharp.Project
+{
+    public class GeneratorsCsProjGenerator : CsProjGenerator
+    {
+        public GeneratorsCsProjGenerator() : base()
+        {
+        }
+
+        public GeneratorsCsProjGenerator(GenGenOptions options, XmlDocument document = null) : base(options, document)
+        {
+        }
+
+        public GeneratorsCsProjGenerator(string filePath) : base(filePath)
+        {
+        }
+
+        public GeneratorsCsProjGenerator(GenGenOptions options) : base(options, "Namespace.GeneratorsName")
+        {
+            
+        }
+    }
+}
---
+++ MetX/MetX.Tests/Standard/Generation/CSharp/Project/GeneratorsCsProjGeneratorTests_FromScratch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using MetX.Aspects;
+using MetX.Standard.Generation.CSharp.Project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MetX.Tests.Standard.Generation.CSharp.Project
+{
+    [TestClass]
+    public class GeneratorsCsProjGeneratorTests_FromScratch
+    {
+        [TestMethod]
+        public void FromScratchXmlIsAsExpected()
+        {
+            var genGenOptions = GenGenOptions.Defaults;
+            genGenOptions.BaseOutputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Product");
+            genGenOptions.TemplatesRootPath = @"..\..\..\..\MetX.Generators\Templates";
+            
+            var generator = new CsProjGenerator(genGenOptions, "Namespace.GeneratorsName");
+
+            Assert.IsNotNull(generator);
+            var actual = generator.Document.OuterXml;
+            Assert.IsFalse(actual.Contains("~~"));
+            
+            Assert.IsTrue(actual.Contains(GenFramework.Standard20.ToTargetFramework()));
+            Assert.IsFalse(actual.Contains("Analyzer"));
+        }
+    }
+}
