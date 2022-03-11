f0507d96bd16b61fdeffc8fa5b7447c1d06bf2b5
William M. Rawls <william.rawls@gmail.com>
2022-03-10 18:06:57 -0800

    Update MenuActionType.cs

---
+++ MetX/MetX.Controls/MenuActionType.cs
@@ -0,0 +1,22 @@
+namespace MetX.Controls
+{
+    public enum MenuActionType
+    {
+        Unknown,
+        None,
+        TechniqueFile,
+        QuickScriptFiles,
+        QuickScriptFile,
+        PipelineFiles,
+        PipelineFile,
+        Connections,
+        Connection,
+        Settings,
+        Directorys,
+        Directory,
+        XslTemplates,
+        XslTemplate,
+        Providers,
+        Provider,
+    }
+}
