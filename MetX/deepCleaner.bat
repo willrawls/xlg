b85771c02999da714a828c863d585cbd4e1e058f
William M. Rawls <william.rawls@gmail.com>
2022-03-29 20:51:51 -0700

    Bank

--- MetX/deepCleaner.bat
+++ MetX/deepCleaner.bat
@@ -1,18 +1,26 @@
 @ECHO OFF
+rmdir /q /s MetX.Five\bin
 rmdir /q /s MetX.Controls\bin
 rmdir /q /s MetX.Glove.Console\bin
+rmdir /q /s MetX.Console.Research\bin
 rmdir /q /s MetX.QuickScripts\bin
 rmdir /q /s MetX.Standard.Primary\bin
 rmdir /q /s MetX.Standard.Library\bin
 rmdir /q /s MetX.Standard.Aspects\bin
 rmdir /q /s MetX.Standard.Generators\bin
+rmdir /q /s MetX.Standard.Generators.Aspects\bin
+rmdir /q /s MetX.Standard.Generators.Support\bin
+rmdir /q /s MetX.Standard.Generators.Actual\bin
 rmdir /q /s MetX.Standard.Generators.Samples.Client\bin
+rmdir /q /s MetX.Standard.XDString\bin
 rmdir /q /s MetX.Techniques.Tests\bin
 rmdir /q /s MetX.Tests\bin
 rmdir /q /s MetX.Windows\bin
 
+rmdir /q /s MetX.Five\obj
 rmdir /q /s MetX.Aspects\obj
 rmdir /q /s MetX.Controls\obj
+rmdir /q /s MetX.Console.Research\obj
 rmdir /q /s MetX.Generators\obj
 rmdir /q /s MetX.Glove.Console\obj
 rmdir /q /s MetX.QuickScripts\obj
@@ -20,6 +28,10 @@ rmdir /q /s MetX.Standard.Primary\obj
 rmdir /q /s MetX.Standard.Library\obj
 rmdir /q /s MetX.Standard.Aspects\obj
 rmdir /q /s MetX.Standard.Generators\obj
+rmdir /q /s MetX.Standard.Generators.Aspects\obj
+rmdir /q /s MetX.Standard.Generators.Support\obj
+rmdir /q /s MetX.Standard.XDString\obj
+rmdir /q /s MetX.Standard.Generators.Actual\obj
 rmdir /q /s MetX.Standard.Generators.Samples.Client\obj
 rmdir /q /s MetX.Techniques.Tests\obj
 rmdir /q /s MetX.Tests\obj
