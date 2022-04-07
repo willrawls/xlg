acb88edc64cf0551373aadb4412feb938d926311
William M. Rawls <william.rawls@gmail.com>
2022-04-07 13:21:59 -0700

    Update Ideas3.cs

--- MetX/MetX.QuickScripts/Walker/Ideas3.cs
+++ MetX/MetX.QuickScripts/Walker/Ideas3.cs
@@ -31,12 +31,24 @@ namespace XLG.QuickScripts.Walker
 
         private void Ideas3_Shown(object sender, System.EventArgs e)
         {
+            UpdateItAll();
+        }
+
+        private void UpdateItAll()
+        {
             RelationshipTreeView.Nodes.Clear();
+
             RelatedNode = RelationshipTreeView.Nodes.Add("Related", "Related");
             LookupNode = RelationshipTreeView.Nodes.Add("Lookup", "Lookup");
             OtherNode = RelationshipTreeView.Nodes.Add("Other", "Other");
+
             TableListView.Items.Clear();
 
+            foreach (var table in XlgDocument.Tables)
+            {
+                ListViewItem listViewItem = new ListViewItem();
+                TableListView.Items.Add(listViewItem);
+            }
         }
 
         public TreeNode RelatedNode { get; set; }
