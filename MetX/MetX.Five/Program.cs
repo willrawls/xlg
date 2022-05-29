918fb6482b491a8160dac64aa8c4b770618b51e6
William M. Rawls <william.rawls@gmail.com>
2022-05-28 18:11:39 -0700

    Before moving classes from Glove to Fiver

--- MetX/MetX.Five/Program.cs
+++ MetX/MetX.Five/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using MetX.Standard.Primary.Interfaces;
 
 namespace MetX.Five
@@ -9,6 +10,12 @@ namespace MetX.Five
         public static void Main(string[] args)
         {
             var arguments = new List<string>(args);
+
+            if (args.Length > 0 && File.Exists(args[0]) && args[0].EndsWith(".xlgq"))
+            {
+                Dirs.LastScriptFilePath = args[0];
+            }
+
             IProcess processorList = new ArgumentListProcessor(arguments);
 
             processorList.Go();
